2f0ef01bc4de9bc4fa55d08c0339ab2c7158dfc6
Adrian <adrian@c-m-d.info>
2021-08-26 13:20:57 +0200

    add api throttle

--- routes/api.php
+++ routes/api.php
@@ -32,16 +32,18 @@ Route::middleware('api_access')
     ->prefix('v1')
     ->group(function () {
         Route::post('node-info', [ServerStatController::class, 'storeNodeInfo'])
-            ->name('post.node-info');
+            ->name('post.node-info')
+            ->middleware('api_throttle:1,300');
         Route::get('node-info', [ServerStatController::class, 'getNodeInfo'])
             ->name('get.node-info');
 
         Route::post('server-stats', [ServerStatController::class, 'storeServerStats'])
-            ->name('post.server-stats');
+            ->name('post.server-stats')
+            ->middleware('api_throttle:1,300');
         Route::get('server-stats', [ServerStatController::class, 'getServerStats'])
             ->name('get.server-stats');
 
-        Route::prefix('webhook')->name('webhook.')->group(function() {
+        Route::prefix('webhook')->name('webhook.')->group(function () {
             Route::post('/', [WebhookController::class, 'create']);
             Route::delete('/', [WebhookController::class, 'delete']);
         });
